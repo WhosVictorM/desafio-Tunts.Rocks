c91e9980878d370ff8512dcd21883a981d66d056
WhosVictorM <victormagalhaespb@gmail.com>
2024-02-14 19:37:49 -0300

    Reading Values and Checking Situations

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const { google } = require("googleapis");
 
+function situationCheck(average, absence) {
+    if(absence <= 15){
+        if(average >= 70){
+            return "Approved"
+        } else if (average >= 50 && average < 70){
+            return "Final Exam"
+        } else {
+            return "Disapproved"
+        }
+    } else {
+        return "Disapproved by absence"
+    }
+}
+
 const auth = new google.auth.GoogleAuth({
   keyFile:
     "C://Users//victo//Desktop//Development//Vscode/TuntsRocks/credentials.json",
@@ -11,3 +25,32 @@ const sheets = google.sheets({ version: "v4", auth });
 const spreadsheetId = "1GNw9IFnNXapd8FcOzcuC11aYLUjQxOmqf4tLT8hkNyo";
 const range = "A4:F27";
 
+sheets.spreadsheets.values.get(
+  {
+    spreadsheetId,
+    range,
+  },
+  (err, res) => {
+    if (err) return console.log("Sheet not found", err);
+
+    const rows = res.data.values;
+
+    if (rows.length) {
+      console.log("Grades: \n");
+      rows.forEach((row => {
+        const id = row[0];
+        const name = row[1];
+        const absence = parseInt(row[2]);
+        const p1 = parseFloat(row[3]);
+        const p2 = parseFloat(row[4]);
+        const p3 = parseFloat(row[5]);
+        const average = (p1 + p2 + p3) / 3;
+        const situation = situationCheck(average, absence)
+
+        console.log(`${name} AVG: ${average.toFixed(2)}\nAbsences: ${absence}\nSituation: ${situation}\n\n`);
+      }))
+    } else {
+      console.log("No Data found");
+    }
+  }
+);
