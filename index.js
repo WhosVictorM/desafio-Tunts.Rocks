a997cc10f07f1a6a9b2af20467fce017fff27bce
WhosVictorM <victormagalhaespb@gmail.com>
2024-02-14 23:24:43 -0300

    Refactored code using Async/Await (Quote limit fixed)

--- index.js
+++ index.js
@@ -14,36 +14,37 @@ function situationCheck(average, absence) {
   }
 }
 
-function calculateNAF(average) {
-  return 100 - average;
-   
+function calculateNAF(average, situation) {
+  if (situation === "Prova Final") {
+    return (100 - average).toFixed(2);
+  } else {
+    return 0;
+  }
 }
 
-const auth = new google.auth.GoogleAuth({
-  keyFile:
-    "C://Users//victo//Desktop//Development//Vscode/TuntsRocks/credentials.json",
-  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-});
+async function main() {
+  const auth = new google.auth.GoogleAuth({
+    keyFile:
+      "C://Users//victo//Desktop//Development//Vscode/TuntsRocks/credentials.json",
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+  });
 
-const sheets = google.sheets({ version: "v4", auth });
+  const sheets = google.sheets({ version: "v4", auth });
 
-const spreadsheetId = "1GNw9IFnNXapd8FcOzcuC11aYLUjQxOmqf4tLT8hkNyo";
-const range = "A4:F27";
+  const spreadsheetId = "1GNw9IFnNXapd8FcOzcuC11aYLUjQxOmqf4tLT8hkNyo";
+  const range = "A4:F27";
 
-sheets.spreadsheets.values.get(
-  {
-    spreadsheetId,
-    range,
-  },
-  (err, res) => {
-    if (err) return console.log("Spreadsheet not found", err);
+  try {
+    const res = await sheets.spreadsheets.values.get({
+      spreadsheetId,
+      range,
+    });
 
     const rows = res.data.values;
 
     if (rows.length) {
       console.log("Grades: \n");
-      rows.forEach((row) => {
-        const id = row[0];
+      for (let row of rows) {
         const name = row[1];
         const absence = parseInt(row[2]);
         const p1 = parseFloat(row[3]);
@@ -52,78 +53,34 @@ sheets.spreadsheets.values.get(
         const average = (p1 + p2 + p3) / 3;
         const situation = situationCheck(average, absence);
 
-        if (situation === "Prova Final") {
-          const naf = calculateNAF(average);
-          console.log(
-            `${name} AVG: ${average.toFixed(
-              2
-            )}\nAbsences: ${absence}\nSituation: ${situation}\nFinal Grade: ${naf.toFixed(2)}\n\n`
-          );
-        } else {
-          console.log(
-            `${name} AVG: ${average.toFixed(
-              2
-            )}\nAbsences: ${absence}\nSituation: ${situation}\nFinal Grade: 0\n\n`
-          );
-        }
+        const naf = calculateNAF(average, situation);
 
-        sheets.spreadsheets.values.update(
-          {
-            spreadsheetId,
-            range: `G${rows.indexOf(row) + 4}`,
-            valueInputOption: "RAW",
-            resource: { values: [[situation]] },
-          },
-          (err, result) => {
-            if (err) {
-              console.log("Unable to Write to Spreadsheet", err);
-            } else {
-              console.log(
-                `Changed Situation Changed - ${name} updated to: ${situation}`
-              );
-            }
-          }
+        console.log(
+          `${name} AVG: ${average.toFixed(
+            2
+          )}\nAbsences: ${absence}\nSituation: ${situation}\nFinal Grade: ${naf}\n\n`
         );
 
-        if(situation === "Prova Final"){
-            sheets.spreadsheets.values.update(
-                {
-                spreadsheetId,
-                range: `H${rows.indexOf(row) + 4}`,
-                valueInputOption: "RAW",
-                resource: { values: [[calculateNAF(average).toFixed(2)]] },
-                },
-                (err, result) => {
-                if (err) {
-                    console.log("Unable to Write to Spreadsheet", err);
-                } else {
-                    console.log(
-                    `NAF from ${name} updated to: ${calculateNAF(average).toFixed(2)}`
-                    );
-                }
-                }
-            )
-        } else {
-            sheets.spreadsheets.values.update(
-                {
-                spreadsheetId,
-                range: `H${rows.indexOf(row) + 4}`,
-                valueInputOption: "RAW",
-                resource: { values: [[0]] },
-                },
-                (err, result) => {
-                if (err) {
-                    console.log("Unable to Write to Spreadsheet", err);
-                } else {
-                    console.log(`NAF from ${name} updated to: 0`);
-                }
-                }
-            )
-        }
+        await sheets.spreadsheets.values.update({
+          spreadsheetId,
+          range: `G${rows.indexOf(row) + 4}`,
+          valueInputOption: "RAW",
+          resource: { values: [[situation]] },
+        });
 
-      });
+        await sheets.spreadsheets.values.update({
+          spreadsheetId,
+          range: `H${rows.indexOf(row) + 4}`,
+          valueInputOption: "RAW",
+          resource: { values: [[naf]] },
+        });
+      }
     } else {
       console.log("No Data found");
     }
+  } catch (err) {
+    console.log("Spreadsheet not found", err);
   }
-);
+}
+
+main();
